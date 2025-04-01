db847d58f7622365962e69cdff3fb8c3a12c1c7c
sohanOvaku <sohanb@ovaku.in>
2025-04-01 12:42:19 +0530

    test docker v1.8

---
+++ app.py
@@ -0,0 +1,15 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+PORT = 8000
+
+class Handler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Hello from Docker v1.0.8!")
+
+if __name__ == "__main__":
+    server = HTTPServer(("0.0.0.0", PORT), Handler)
+    print(f"Serving on port {PORT}")
+    server.serve_forever()
