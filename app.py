72b07a5cd51b49d667fc3187211abc30b46591ec
sohanOvaku <sohanb@ovaku.in>
2025-04-01 14:33:46 +0530

    v1.0.11

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 
-PORT = 8080
+PORT = 8000
 
 class Handler(SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
-        self.wfile.write(b"Hello from Docker v1.0.8!")
+        self.wfile.write(b"Hello from Docker v1.0.11!")
 
 if __name__ == "__main__":
     server = HTTPServer(("0.0.0.0", PORT), Handler)
